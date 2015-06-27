440b73fe9b4c7b0f287f48390a8066ee6fa0a773
W. Nicholas Greene <wng@csail.mit.edu>
2015-06-26 22:46:30 -0400

    Fix template installation.

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ setup.py for installing metaproject.
 """
 
 from distutils.core import setup
+import os
+
+DATADIR = 'templates'
+DATAFILES = [(root, [os.path.join(root, f) for f in files])
+             for root, dirs, files in os.walk(DATADIR)]
 
 setup(name="metaproject",
       version="0.1.0",
@@ -12,5 +17,5 @@ setup(name="metaproject",
       author="W. Nicholas Greene",
       author_email="wng@csail.mit.edu",
       url="https://github.mit.edu/rrg/metaproject",
-      package_data={'': ['template/*']},
+      data_files=DATAFILES,
       scripts=['metaproject.py'])
