47a9a55a323f84057b46a23d36d6fff32448d66c
William Nicholas Greene <wng@csail.mit.edu>
2015-06-28 03:50:29 -0400

    Remove templates from setup.py.

--- setup.py
+++ setup.py
@@ -5,11 +5,6 @@ setup.py for installing projector.
 """
 
 from distutils.core import setup
-import os
-
-DATADIR = 'templates'
-DATAFILES = [(root, [os.path.join(root, f) for f in files])
-             for root, dirs, files in os.walk(DATADIR)]
 
 setup(name="projector",
       version="0.1.0",
@@ -17,5 +12,4 @@ setup(name="projector",
       author="W. Nicholas Greene",
       author_email="wng@csail.mit.edu",
       url="https://github.mit.edu/rrg/projector",
-      data_files=DATAFILES,
       scripts=['projector.py'])
